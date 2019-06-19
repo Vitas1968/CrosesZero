d854b6912a5efb67b791fc7774fe38178249f462
Vitas1968 <dor1968@mail.ru>
2019-06-19 18:23:22 +0700

    Инициализируем данные перед началом игры

---
+++ src/TicTacToe.java
@@ -0,0 +1,12 @@
+public class TicTacToe
+{
+    private  static char [][] map; //игровое поле, матрица игры
+    private  static int size = 3; // размер поля
+    private  static char DOT_EMTY = '*'; // пустое поле
+    private  static char DOT_X = 'X';   // символ Х
+    private  static char DOT_O = 'O';   //  символ О
+    public static void main(String[] args)
+    {
+
+    }
+}
