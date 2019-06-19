7dfa75eae856983260082fc9cfd25cd2e050e27e
Vitas1968 <dor1968@mail.ru>
2019-06-19 19:26:25 +0700

    Добавлено I

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,6 +6,7 @@ public class TicTacToe
     private  static char DOT_X = 'X';   // символ Х
     private  static char DOT_O = 'O';   //  символ О
     private  static char DOT_K = 'K';   //  проба
+    private  static char DOT_I = 'I';   //  dfgsdg
 
     public static void main(String[] args)
     {
