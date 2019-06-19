94468cdf2b66f968de23505f6bb0bc78d34ec25b
Vitas1968 <dor1968@mail.ru>
2019-06-19 19:34:29 +0700

    Update and rename src/TicTacToe.java to  SRC/TicTacToe.java
    Удалил лишнее

--- SRC/TicTacToe.java
+++ SRC/TicTacToe.java
@@ -5,9 +5,6 @@ public class TicTacToe
     private  static char DOT_EMTY = '*'; // пустое поле
     private  static char DOT_X = 'X';   // символ Х
     private  static char DOT_O = 'O';   //  символ О
-    private  static char DOT_K = 'K';   //  проба
-    private  static char DOT_I = 'I';   //  dfgsdg
-
     public static void main(String[] args)
     {
 
